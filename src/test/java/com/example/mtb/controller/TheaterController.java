6d6a8472b83f37a38b06031373a44d629600600e
unknown <sameekshaacharya2003@gmail.com>
2025-04-30 11:45:38 +0530

    UserDetails security Configuration

--- src/test/java/com/example/mtb/controller/TheaterController.java
+++ src/test/java/com/example/mtb/controller/TheaterController.java
@@ -6,10 +6,10 @@ import com.example.mtb.service.TheaterService;
 import com.example.mtb.util.ResponseStructure;
 import com.example.mtb.util.RestResponseBuilder;
 import jakarta.validation.Valid;
-
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
@@ -21,6 +21,7 @@ public class TheaterController {
     private final RestResponseBuilder responseBuilder;
 
     @PostMapping("/theaters")
+    @PreAuthorize("hasAuthority('THEATER_OWNER')")
     public ResponseEntity<ResponseStructure<TheaterResponse>> addTheater(String email, @Valid @RequestBody TheaterRequest theaterRequest){
         TheaterResponse theaterResponse = theaterService.addTheater(email, theaterRequest);
         return responseBuilder.sucess(HttpStatus.OK, "Theater has been succesfull created", theaterResponse);
@@ -33,6 +34,7 @@ public class TheaterController {
     }
 
     @PutMapping("/theaters/{theaterId}")
+    @PreAuthorize("hasAuthority('THEATER_OWNER')")
     public ResponseEntity<ResponseStructure<TheaterResponse>> updateTheater(@PathVariable String theaterId, @Valid @RequestBody TheaterRequest registerationRequest){
         TheaterResponse theaterResponse = theaterService.updateTheater(theaterId, registerationRequest);
         return responseBuilder.sucess(HttpStatus.OK, "Theater has been sucessfully Updated", theaterResponse);
